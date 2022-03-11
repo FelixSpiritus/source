b2af9c293bdc7fd3a09f1908a549431a7ab29854
FelixSpiritus <dkakorin@gmail.com>
2022-03-12 01:47:09 +0300

    MVC

---
+++ ASP.MVC/WebMVCR1.LAB2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVCR1.Models;
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index(string hel)
+        {
+            //int hour = DateTime.Now.Hour;
+            //string Greeting = hour < 12 ? "Доброе утро" : "Добрый день" + ", " + hel;
+            //return Greeting;
+            
+            //string Greeting = ModelClass.ModelHello() + ", " + hel;
+            //return Greeting;
+            string res = ExeEnum();
+            return res;
+
+        }
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            string res1 = String.Format("Тип банковского счета {0}", goldAccount);
+            string res2 = String.Format("Тип банковского счета {0}", platinumAccount);
+            string res = res1 + "<p>" + res2;
+            return res;
+        }
+
+    }
+}
---
+++ ASP.MVC/WebMVCR1.LAB2/Models/StudyCsharp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVCR1.Models
+{
+    public enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+    public class StudyCsharp
+    {
+    }
+}
