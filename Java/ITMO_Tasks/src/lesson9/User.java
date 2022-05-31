8f8cd67a7894172de8a72344132ada90a5d7db9b
FelixSpiritus <dkakorin@gmail.com>
2022-06-01 02:03:53 +0300

    01.06.2022

--- Java/ITMO_Tasks/src/lesson9/User.java
+++ Java/ITMO_Tasks/src/lesson9/User.java
@@ -47,7 +47,7 @@ public class User {
     }
 
     public static void main(String[] args) {
-        Map map = new HashMap();
+        Map<User, Integer> map = new HashMap<>();
         fillMap(map);
         showUsePoints(map);
     }
--- Java/ITMO_Tasks/src/lesson9/ex2.java
+++ Java/ITMO_Tasks/src/lesson9/ex2.java
@@ -5,11 +5,7 @@ import java.util.*;
 public class ex2 {
 
     public static Set<String> noDouble(List<String> string){
-        Set<String> set = new HashSet<>();
-        for (String str: string){
-            set.add(str);
-        }
-        return set;
+        return new HashSet<>(string);
     }
 
     public static void main(String[] args) {
