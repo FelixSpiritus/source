45c44dd9b42d44656953c0480e4ad2baeae63ca6
FelixSpiritus <dkakorin@gmail.com>
2022-07-11 01:28:14 +0300

    11.07.2022

--- Cpp/CppItmoTasks/SchoolCpp/main.cpp
+++ Cpp/CppItmoTasks/SchoolCpp/main.cpp
@@ -22,15 +22,14 @@ int main()
 	scores.push_back(3);
 	scores.push_back(3);
 	student* stud = new student("Петров", "Иван", "Алексеевич", scores);
-	human* hum = stud;
 	stud->get_full_name();
 	std::cout << "Средний балл : " << stud->get_average_score() << std::endl;
 	unsigned int teacher_work_time = 40;
 	teacher* tch = new teacher("Сергеев", "Дмитрий", "Сергеевич", teacher_work_time);
 	tch->get_full_name();
 	std::cout << "Количество часов: " << tch->get_work_time() << std::endl;
-	tch->get_full_name();
-	stud->get_full_name();
+	std::cout << endl;
+	human* hum = stud;
 	hum->get_full_name();
 	return 0;
 }
